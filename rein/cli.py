6a1ebfc2c447991d5cb62cd3b2621103524fc105
David Sterry <davids@exchb.com>
2015-12-27 01:07:05 -0800

    move create and import code to user module

--- rein/cli.py
+++ rein/cli.py
@@ -8,7 +8,7 @@ from subprocess import check_output
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from lib.user import User, Base
+from lib.user import User, Base, create_account, import_account
  
 
 db_filename = 'local.db'
@@ -30,48 +30,39 @@ def yes_or_no(question):
 
 #@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
 #@click.argument('name', default='world', required=False)
-@click.command()
+@click.group()
+def cli():
+    pass
+
+@cli.command()
 def main():
-    '''Rein CLI'''
-    click.echo("\nWelcome to Rein, the decentralized services marketplace.\n")
+    '''Rein - decentralized professional services market'''
+    click.echo("\nWelcome to Rein.\n")
     if not os.path.isfile(db_filename):
-        click.echo("It looks like this is your first time running Rein.\nDo you want to import a backup or create a new account?\n1 - Import\n2 - Create\n")
+        click.echo("It looks like this is your first time running Rein on this computer.\n"\
+                "Do you want to import a backup or create a new account?\n"\
+                "1 - Import\n2 - Create\n")
         choice = 0
         while choice not in (1, 2):
-
             choice = click.prompt("Choice", type=int, default=2)
             if choice == 2:
-                Base.metadata.create_all(engine)
-                name = click.prompt("Enter name or handle", type=str)
-                contact = click.prompt("Enter email or BitMessage address", type=str)
-
-                new_identity = User(name, contact)
-                session.add(new_identity)
-                session.commit()
-
-                data = {'name': name, 'contact': contact}
-                backup_filename = 'rein-backup.json'
-                if not os.path.isfile(backup_filename):
-                    f = open(backup_filename,'w')
-                    try:
-                        f.write(json.dumps(data))
-                        click.echo("Backup saved successfully to %s" % backup_filename)
-                    except:
-                        raise RuntimeError('Problem writing user details to json backup file.')
-                    f.close()
-                else:
-                    click.echo("Backup flie already exists. Please run with --backup to save user details to file.")
-#u = User(name, email)
+                create_account(engine, session, name, contact)
             elif choice == 1:
-                Base.metadata.create_all(engine)
-                backup_filename = click.prompt("Enter backup file name: (rein-backup.json)", type=str, default='rein-backup.json')    
-                f = open(backup_filename, 'r')
-                try:
-                    data = json.loads(f.read())
-                except:
-                    raise RuntimeError('Backup file %s not valid json.' % backup_filename)
+                import_account(engine, session)
+    else:
+        bold = '\033[1m'
+        regular = '\033[0m'
+        click.echo("""Available commands:
 
-                new_identity = User(data['name'], data['contact'])
-                session.add(new_identity)
-                session.commit()
-                
+    info
+    sync
+    post-job
+    post-listing
+    post-bid
+    post-offer
+    accept-bid
+    post-work
+    accept-work
+
+For more info visit http://reinproject.org
+                """)
--- rein/lib/user.py
+++ rein/lib/user.py
@@ -17,3 +17,37 @@ class User(Base):
         self.name = name
         self.contact = contact
 
+
+def create_account(engine, session, name, contact):
+    Base.metadata.create_all(engine)
+    name = click.prompt("Enter name or handle", type=str)
+    contact = click.prompt("Enter email or BitMessage address", type=str)
+    new_identity = User(name, cnentact)
+    session.add(new_identity)
+    session.commit()
+
+    data = {'name': name, 'contact': contact}
+    backup_filename = 'rein-backup.json'
+    if not os.path.isfile(backup_filename):
+        f = open(backup_filename,'w')
+        try:
+            f.write(json.dumps(data))
+            click.echo("Backup saved successfully to %s" % backup_filename)
+        except:
+            raise RuntimeError('Problem writing user details to json backup file.')
+        f.close()
+    else:
+        click.echo("Backup flie already exists. Please run with --backup to save "\
+                   "user details to file.")
+
+def import_account(engine, session):
+    Base.metadata.create_all(engine)
+    backup_filename = click.prompt("Enter backup file name", type=str, default='rein-backup.json')    
+    f = open(backup_filename, 'r')
+    try:
+        data = json.loads(f.read())
+    except:
+        raise RuntimeError('Backup file %s not valid json.' % backup_filename)
+    new_identity = User(data['name'], data['contact'])
+    session.add(new_identity)
+    session.commit()
