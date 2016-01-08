e6c9b88d11935cfb06f772cf588978c6f92c8fbb
David Sterry <dsterry@ext.uber.com>
2016-01-08 08:44:09 -0800

    flake8 leaving E781, E128

--- rein/lib/bitcoinsig.py
+++ rein/lib/bitcoinsig.py
@@ -14,26 +14,27 @@ _b = 0x0000000000000000000000000000000000000000000000000000000000000007L
 _a = 0x0000000000000000000000000000000000000000000000000000000000000000L
 _Gx = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798L
 _Gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8L
-curve_secp256k1 = ecdsa.ellipticcurve.CurveFp( _p, _a, _b )
-generator_secp256k1 = ecdsa.ellipticcurve.Point( curve_secp256k1, _Gx, _Gy, _r )
-oid_secp256k1 = (1,3,132,0,10)
-SECP256k1 = ecdsa.curves.Curve("SECP256k1", curve_secp256k1, generator_secp256k1, oid_secp256k1 ) 
+curve_secp256k1 = ecdsa.ellipticcurve.CurveFp(_p, _a, _b)
+generator_secp256k1 = ecdsa.ellipticcurve.Point(curve_secp256k1, _Gx, _Gy, _r)
+oid_secp256k1 = (1, 3, 132, 0, 10)
+SECP256k1 = ecdsa.curves.Curve("SECP256k1", curve_secp256k1, generator_secp256k1, oid_secp256k1)
 
 addrtype = 0
 
 # from http://eli.thegreenplace.net/2009/03/07/computing-modular-square-roots-in-python/
 
+
 def modular_sqrt(a, p):
     """ Find a quadratic residue (mod p) of 'a'. p
     must be an odd prime.
-    
+
     Solve the congruence of the form:
     x^2 = a (mod p)
     And returns x. Note that p - x is also a root.
-    
+
     0 is returned is no square root exists for
     these a and p.
-    
+
     The Tonelli-Shanks algorithm is used (except
     for some simple cases in which the solution
     is known from an identity). This algorithm
@@ -50,7 +51,7 @@ def modular_sqrt(a, p):
         return p
     elif p % 4 == 3:
         return pow(a, (p + 1) / 4, p)
-    
+
     # Partition p-1 to s * 2^e for an odd s (i.e.
     # reduce all the powers of 2 from p-1)
     #
@@ -59,20 +60,20 @@ def modular_sqrt(a, p):
     while s % 2 == 0:
         s /= 2
         e += 1
-        
+
     # Find some 'n' with a legendre symbol n|p = -1.
     # Shouldn't take long.
     #
     n = 2
     while legendre_symbol(n, p) != -1:
         n += 1
-        
+
     # Here be dragons!
     # Read the paper "Square roots from 1; 24, 51,
     # 10 to Dan Shanks" by Ezra Brown for more
     # information
     #
-    
+
     # x is a guess of the square root that gets better
     # with each iteration.
     # b is the "fudge factor" - by how much we're off
@@ -86,7 +87,7 @@ def modular_sqrt(a, p):
     b = pow(a, s, p)
     g = pow(n, s, p)
     r = e
-    
+
     while True:
         t = b
         m = 0
@@ -94,22 +95,23 @@ def modular_sqrt(a, p):
             if t == 1:
                 break
             t = pow(t, 2, p)
-            
+
         if m == 0:
             return x
-        
+
         gs = pow(g, 2 ** (r - m - 1), p)
         g = (gs * gs) % p
         x = (x * gs) % p
         b = (b * g) % p
         r = m
-        
+
+
 def legendre_symbol(a, p):
     """ Compute the Legendre symbol a|p using
     Euler's criterion. p is a prime, a is
     relatively prime to p (if p divides
     a, then a|p = 0)
-    
+
     Returns 1 if a has a square root modulo
     p, -1 otherwise.
     """
@@ -119,8 +121,9 @@ def legendre_symbol(a, p):
 __b58chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 __b58base = len(__b58chars)
 
+
 def b58encode(v):
-    """ encode v, which is a string of bytes, to base58.		
+    """ encode v, which is a string of bytes, to base58.
     """
 
     long_value = 0L
@@ -143,36 +146,42 @@ def b58encode(v):
 
     return (__b58chars[0]*nPad) + result
 
+
 def decbin(d, l=0, rev=False):
-	if l==0:
-		a="%x"%d
-		if len(a)%2: a='0'+a
-	else:
-		a=("%0"+str(2*l)+"x")%d
-	a=a.decode('hex')
-	if rev:
-		a=a[::-1]
-	return a
+    if l == 0:
+        a = "%x" % d
+        if len(a) % 2: a = '0' + a
+    else:
+        a = ("%0" + str(2 * l) + "x") % d
+    a = a.decode('hex')
+    if rev:
+        a = a[::-1]
+    return a
+
 
 def decvi(d):
-	if d<0xfd:
-		return decbin(d)
-	elif d<0xffff:
-		return '\xfd'+decbin(d,2,True)
-	elif d<0xffffffff:
-		return '\xfe'+decbin(d,4,True)
-	return '\xff'+decbin(d,8,True)
+    if d < 0xfd:
+        return decbin(d)
+    elif d < 0xffff:
+        return '\xfd' + decbin(d, 2, True)
+    elif d < 0xffffffff:
+        return '\xfe' + decbin(d, 4, True)
+    return '\xff' + decbin(d, 8, True)
+
 
 def msg_magic(msg):
-    return "\x18Bitcoin Signed Message:\n"+decvi(len(msg))+msg
+    return "\x18Bitcoin Signed Message:\n" + decvi(len(msg)) + msg
+
 
 def Hash(data):
     return hashlib.sha256(hashlib.sha256(data).digest()).digest()
+
 
 def hash_160(public_key):
     md = hashlib.new('ripemd160')
     md.update(hashlib.sha256(public_key).digest())
     return md.digest()
+
 
 def hash_160_to_bc_address(h160):
     vh160 = chr(addrtype) + h160
@@ -180,9 +189,11 @@ def hash_160_to_bc_address(h160):
     addr = vh160 + h[0:4]
     return b58encode(addr)
 
+
 def public_key_to_bc_address(public_key):
     h160 = hash_160(public_key)
     return hash_160_to_bc_address(h160)
+
 
 def encode_point(pubkey, compressed=False):
     order = generator_secp256k1.order()
@@ -194,23 +205,25 @@ def encode_point(pubkey, compressed=False):
     else:
         return chr(4) + x_str + y_str
 
+
 def sign_message(private_key, message, compressed=False):
     public_key = private_key.get_verifying_key()
-    signature = private_key.sign_digest( Hash( msg_magic( message ) ), sigencode = ecdsa.util.sigencode_string )
+    signature = private_key.sign_digest(Hash(msg_magic(message)), sigencode=ecdsa.util.sigencode_string)
     address = public_key_to_bc_address(encode_point(public_key, compressed))
-    assert public_key.verify_digest( signature, Hash( msg_magic( message ) ), sigdecode = ecdsa.util.sigdecode_string)
+    assert public_key.verify_digest(signature, Hash(msg_magic(message)), sigdecode=ecdsa.util.sigdecode_string)
     for i in range(4):
         nV = 27 + i
         if compressed:
             nV += 4
-        sig = base64.b64encode( chr(nV) + signature )
+        sig = base64.b64encode(chr(nV) + signature)
         try:
-            if verify_message( address, sig, message):
+            if verify_message(address, sig, message):
                 return sig
         except:
             continue
     else:
         raise BaseException("error: cannot sign message")
+
 
 def verify_message(address, signature, message):
     """ See http://www.secg.org/download/aid-780/sec1-v2.pdf for the math """
@@ -221,7 +234,7 @@ def verify_message(address, signature, message):
     # extract r,s from signature
     sig = base64.b64decode(signature)
     if len(sig) != 65: raise BaseException("Wrong encoding")
-    r,s = util.sigdecode_string(sig[1:], order)
+    r, s = util.sigdecode_string(sig[1:], order)
     nV = ord(sig[0])
     if nV < 27 or nV >= 35:
         return False
@@ -234,41 +247,42 @@ def verify_message(address, signature, message):
     # 1.1
     x = r + (recid/2) * order
     # 1.3
-    alpha = ( x * x * x  + curve.a() * x + curve.b() ) % curve.p()
+    alpha = (x * x * x + curve.a() * x + curve.b()) % curve.p()
     beta = modular_sqrt(alpha, curve.p())
     y = beta if (beta - recid) % 2 == 0 else curve.p() - beta
     # 1.4 the constructor checks that nR is at infinity
     R = ellipticcurve.Point(curve, x, y, order)
     # 1.5 compute e from message:
-    h = Hash( msg_magic( message ) )
+    h = Hash(msg_magic(message))
     e = string_to_number(h)
     minus_e = -e % order
     # 1.6 compute Q = r^-1 (sR - eG)
-    inv_r = numbertheory.inverse_mod(r,order)
-    Q = inv_r * ( s * R + minus_e * G )
-    public_key = ecdsa.VerifyingKey.from_public_point( Q, curve = SECP256k1 )
+    inv_r = numbertheory.inverse_mod(r, order)
+    Q = inv_r * (s * R + minus_e * G)
+    public_key = ecdsa.VerifyingKey.from_public_point(Q, curve=SECP256k1)
     # check that Q is the public key
-    public_key.verify_digest( sig[1:], h, sigdecode = ecdsa.util.sigdecode_string)
+    public_key.verify_digest(sig[1:], h, sigdecode=ecdsa.util.sigdecode_string)
     # check that we get the original signing address
     addr = public_key_to_bc_address(encode_point(public_key, compressed))
     if address == addr:
         return True
     else:
-        #print addr
+        # print addr
         return False
-        
+
+
 if __name__ == '__main__':
     # some simple testing code
     print verify_message('16vqGo3KRKE9kTsTZxKoJKLzwZGTodK3ce',
             'HPDs1TesA48a9up4QORIuub67VHBM37X66skAYz0Esg23gdfMuCTYDFORc6XGpKZ2/flJ2h/DUF569FJxGoVZ50=',
-            'test message') # good
+            'test message')  # good
     print verify_message('16vqGo3KRKE9kTsTZxKoJKLzwZGTodK3ce',
             'HPDs1TesA48a9up4QORIuub67VHBM37X66skAYz0Esg23gdfMuCTYDFORc6XGpKZ2/flJ2h/DUF569FJxGoVZ50=',
-            'test message 2') # bad
+            'test message 2')  # bad
 
-    private_key = ecdsa.SigningKey.from_string( '5JkuZ6GLsMWBKcDWa5QiD15Uj467phPR', curve = SECP256k1 )
+    private_key = ecdsa.SigningKey.from_string('5JkuZ6GLsMWBKcDWa5QiD15Uj467phPR', curve=SECP256k1)
     public_key = private_key.get_verifying_key()
-    bitcoinaddress = public_key_to_bc_address( encode_point(public_key) )
+    bitcoinaddress = public_key_to_bc_address(encode_point(public_key))
     print bitcoinaddress
     sig = sign_message(private_key, 'test message')
     print sig
@@ -287,9 +301,9 @@ if __name__ == '__main__':
     # sign compressed key
     compressed = True
     secret = 'dea7715ddcf5aba27530d6a1393813fbdd09af3aeb5f4f1616f563833d07babb'.decode('hex')
-    private_key = ecdsa.SigningKey.from_string( secret, curve = SECP256k1 )
+    private_key = ecdsa.SigningKey.from_string(secret, curve=SECP256k1)
     public_key = private_key.get_verifying_key()
-    bitcoinaddress = public_key_to_bc_address( encode_point(public_key, compressed) )
+    bitcoinaddress = public_key_to_bc_address(encode_point(public_key, compressed))
     print bitcoinaddress
 
     sig = sign_message(private_key, 'test message', compressed)
@@ -300,4 +314,3 @@ if __name__ == '__main__':
     print verify_message('1LsPb3D1o1Z7CzEt1kv5QVxErfqzXxaZXv',
             'H3I37ur48/fn52ZvWQT+Mj2wXL36gyjfaN5qcgfiVRTJb1eP1li/IacCQspYnUntiRv8r6GDfJYsdiQ5VzlG3As=',
             'testtest')
-
