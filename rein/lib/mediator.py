5695ecbac6ccd1a7d892e631432a56938ada44c3
David Sterry <dsterry@uber.com>
2016-08-24 17:43:56 -0700

    clean up

--- rein/lib/mediator.py
+++ rein/lib/mediator.py
@@ -1,21 +1,13 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, and_
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import requests
 import click
 import config
-#import os
 from validate import filter_and_parse_valid_sigs
 from market import unique
 
 Base = declarative_base()
 rein = config.Config()
-
-#engine = create_engine("sqlite:///%s" % os.path.join(rein.config_dir, rein.db_filename))
-#Base.metadata.bind = engine
-#DBSession = sessionmaker(bind=engine)
-#session = DBSession()
 
 class Mediator(Base):
     __tablename__ = 'mediator'
@@ -50,28 +42,3 @@ class Mediator(Base):
             return ret
         else:
             return rein.session.query(Mediator).filter(Mediator.maddr == maddr).all()
-
-def get_mediators_bad(rein, user, urls, log):
-    eligible_mediators = []
-    blocks = []
-    for url in urls:
-        log.info("Querying %s for mediators..." % url)
-        sel_url = "{0}query?owner={1}&query=mediators&testnet={2}"
-        try:
-            answer = requests.get(url=sel_url.format(url, user.maddr, rein.testnet))
-        except:
-            click.echo('Error connecting to server.')
-            log.error('server connect error ' + url)
-            continue
-        data = answer.json()
-        if len(data['mediators']) == 0:
-            click.echo('None found')
-        if data['block_info']:
-            blocks.append(data['block_info'])
-        eligible_mediators += filter_and_parse_valid_sigs(rein, data['mediators'])
-    mediators = unique(eligible_mediators, 'Mediator public key')
-    objs = []
-    for m in mediators:
-        newMediator = Mediator(m[u'User'], m[u'Master signing address'], m[u'Mediator public key'], m[u'Mediator fee'])
-        objs.append(newMediator)
-    return objs
