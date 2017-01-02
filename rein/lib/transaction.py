eb38a437aad50d5f29dc983ad452b19ef6b53318
Andrew K <general@akrmn.org>
2017-01-02 06:09:16 +0000

    fix up transaction module

--- rein/lib/transaction.py
+++ rein/lib/transaction.py
@@ -3,17 +3,19 @@ from bitcoin.core import b2x, lx, COIN, COutPoint, CMutableTxOut, CMutableTxIn, 
 from bitcoin.core.script import CScript, OP_DUP, OP_HASH160, OP_EQUALVERIFY, OP_CHECKSIG, SignatureHash, SIGHASH_ALL, OP_2, OP_3, OP_CHECKMULTISIG, OP_0
 from bitcoin.core.scripteval import VerifyScript, SCRIPT_VERIFY_P2SH
 from bitcoin.wallet import CBitcoinAddress, CBitcoinSecret
-SelectParams("testnet") #tmp
 import urllib2, urllib
 import json
 from hashlib import sha256
-api = "blocktrail"
+api = "blocktrail" #handle this
 from .bucket import Bucket
 from .io import safe_get
 
-def unspent_txins(address):
+def unspent_txins(address,testnet):
     if (api == "blockr"):
-        url = "https://tbtc.blockr.io/api/v1/address/unspent/"+str(address)
+        if testnet:
+            url = "https://tbtc.blockr.io/api/v1/address/unspent/"+str(address)
+        else:
+            url = "https://btc.blockr.io/api/v1/address/unspent/"+str(address)
         hdr = {'User-Agent': 'Mozilla/5.0'}
         request = urllib2.Request(url,headers=hdr)
         response = urllib2.urlopen(request)
@@ -27,7 +29,10 @@ def unspent_txins(address):
             total_value += value;
             txins.append((txid, vout))
     else:
-        url = "https://api.blocktrail.com/v1/tbtc/address/"+str(address)+"/unspent-outputs?api_key=1e1ebd7ae629e031310ae9d61fe8549c82d0c589"
+        if testnet:
+            url = "https://api.blocktrail.com/v1/tbtc/address/"+str(address)+"/unspent-outputs?api_key=1e1ebd7ae629e031310ae9d61fe8549c82d0c589"
+        else:
+            url = "https://api.blocktrail.com/v1/btc/address/"+str(address)+"/unspent-outputs?api_key=1e1ebd7ae629e031310ae9d61fe8549c82d0c589"
         hdr = {'User-Agent': 'Mozilla/5.0'}
         request = urllib2.Request(url,headers=hdr)
         response = urllib2.urlopen(request)
@@ -49,26 +54,17 @@ def broadcast_tx (tx_hex,rein):
     sel_url = "{0}bitcoin?owner={1}&query=sendrawtransaction&tx={2}&testnet={3}"
     
     for url in urls:
-        print("get url: "+url)
         data = safe_get(rein.log, sel_url.format(url,rein.user.maddr,tx_hex,rein.testnet))
-        print("got url")
         if data and 'txid' in data:
             print("got data and txid")
             return data['txid']
-        elif data and 'message' in data:
-            print("no txid")
-            print("message: "+data['message'])
-        elif data:
-            print("yes data")
-        else:
-            print("no data")
     
 def partial_spend_p2sh (redeemScript,rein):
     daddr = rein.user.daddr
     txin_redeemScript = CScript(x(redeemScript))
     txin_scriptPubKey = txin_redeemScript.to_p2sh_scriptPubKey()
     txin_p2sh_address = CBitcoinAddress.from_scriptPubKey(txin_scriptPubKey)
-    (txins,total_value) = unspent_txins(txin_p2sh_address)
+    (txins,total_value) = unspent_txins(txin_p2sh_address,rein.testnet)
     if len(txins)==0:
         raise ValueError('No unspent txins found')
     txins_str = ""
@@ -88,10 +84,6 @@ def partial_spend_p2sh (redeemScript,rein):
     for i in range(0,ntxins):
         sighash = SignatureHash(txin_redeemScript, tx, i, SIGHASH_ALL)
         sig += " "+b2x(seckey.sign(sighash))+"01"
-    print("txins_str = "+txins_str[1:])
-    print("amount = "+str(amount))
-    print("daddr = "+daddr)
-    print("sig = "+sig)
     return (txins_str[1:],str(amount),daddr,sig)
 
 def spend_p2sh (redeemScript,txins_str,amount,daddr,sig,rein):
@@ -117,9 +109,7 @@ def spend_p2sh (redeemScript,txins_str,amount,daddr,sig,rein):
     hash = sha256(sha256(tx_bytes).digest()).digest()
     txid = b2x(hash[::-1])
     txid_causeway = broadcast_tx(b2x(tx_bytes),rein)
-    if (txid != txid_causeway):
-        print("txids don't match")    
-    return txid_causeway
+    return (txid,sig)
 
 #redeemScript = "522103220d8573c2d9bdea5d60d3ad8a892c94da4a1850445fe3b83b17307a8d655fb1210342b976a71a5aa2daa65512da45f1d44579c1d3fc364d085e398fc771c23fe2622102ba974cf1e3853814e0e05e7e36266e3d5d324eb48a31694d4969bed390535a6053ae"
 
