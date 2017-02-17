be04b750141c94796e5ee1faaf3b565f89b306b9
FreakJoe <sandmensch933@web.de>
2017-02-17 18:28:44 +0100

    Open correct localization file

--- rein/lib/localization.py
+++ rein/lib/localization.py
@@ -1,19 +1,23 @@
 import gettext
 import locale
 import logging
+import os
+import rein.locale
 
 def init_localization():
   '''prepare l10n'''
   locale.setlocale(locale.LC_ALL, '') # use user's preferred locale
   # take first two characters of country code
   loc = locale.getlocale()
-  filename = "res/messages_%s.mo" % locale.getlocale()[0][0:2]
+  file_name = "messages-%s.mo" % locale.getlocale()[0][0:2]
+  file_path = os.path.join(os.path.dirname(os.path.abspath(rein.locale.__file__)), file_name)
+  print(file_path)
 
   try:
-    logging.debug( "Opening message file %s for locale %s", filename, loc[0] )
-    trans = gettext.GNUTranslations(open( filename, "rb" ) )
+    logging.debug("Opening message file %s for locale %s", file_name, loc[0])
+    trans = gettext.GNUTranslations(open(file_path, "rb" ))
   except IOError:
-    logging.debug( "Locale not found. Using default messages" )
+    logging.debug("Locale not found. Using default messages")
     trans = gettext.NullTranslations()
 
   trans.install()
--- rein/locale/compile.py
+++ rein/locale/compile.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
+# Compile all .po files within the current working directory to .mo format
 for f in os.listdir(os.getcwd()):
 	if f.endswith('.po'):
 		subprocess.call("msgfmt {} -o {}".format(f, f.replace('.po', '.mo')))
