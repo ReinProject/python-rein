0743cebd10c86d5f8a19f508830e69edfd33e66f
FreakJoe <sandmensch933@web.de>
2017-02-19 14:07:17 +0100

    Properly extract system locale, remove unnecessary file

--- rein/lib/localization.py
+++ rein/lib/localization.py
@@ -6,16 +6,16 @@ import rein.locale
 
 def init_localization():
   '''prepare l10n'''
-  locale.setlocale(locale.LC_ALL, '') # use user's preferred locale
-  # take first two characters of country code
-  loc = locale.getlocale()
-  file_name = "messages-%s.mo" % locale.getlocale()[0][0:2]
+  # Extract system locale identifier
+  loc = locale.getdefaultlocale()[0]
+  file_name = "messages-%s.mo" % loc.split('_')[0]
   file_path = os.path.join(os.path.dirname(os.path.abspath(rein.locale.__file__)), file_name)
   print(file_path)
 
   try:
     logging.debug("Opening message file %s for locale %s", file_name, loc[0])
     trans = gettext.GNUTranslations(open(file_path, "rb" ))
+
   except IOError:
     logging.debug("Locale not found. Using default messages")
     trans = gettext.NullTranslations()
